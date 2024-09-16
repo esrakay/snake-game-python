9c61d82701feee459a99818172a8dfd22ce6a6fd
Esra Akay <esra.akay67@icloud.com>
2024-09-16 21:33:53 +0200

    Add main file

---
+++ main.py
@@ -0,0 +1,29 @@
+import time
+from turtle import Screen
+from snake import Snake
+
+
+screen = Screen()
+screen.setup(600, 600)
+screen.bgcolor("black")
+screen.title("Snake Game")
+screen.tracer(0)
+screen.listen()
+
+
+snake = Snake()
+screen.onkey(snake.right, "d")
+screen.onkey(snake.up, "w")
+screen.onkey(snake.left, "a")
+screen.onkey(snake.down, "s")
+
+while True:
+    snake.move()
+    screen.update()
+    time.sleep(0.07)
+
+
+screen.exitonclick()
+
+if __name__ == "__main__":
+    main()
