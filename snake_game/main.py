0af65ec692c23c9fb288c3c6987f41ece95b344c
Esra Akay <esra.akay67@icloud.com>
2024-09-17 00:49:00 +0200

    Restructure code

--- snake_game/main.py
+++ snake_game/main.py
@@ -3,27 +3,38 @@ from turtle import Screen
 from snake_game.classes.snake import Snake
 
 
-screen = Screen()
-screen.setup(500, 500)
-screen.bgcolor("black")
-screen.title("Snake Game")
-screen.tracer(0)
-screen.listen()
+def set_screen():
+    screen = Screen()
+    screen.setup(500, 500)
+    screen.bgcolor("black")
+    screen.title("Snake Game")
+    screen.tracer(0)
+    screen.listen()
+    return screen
 
 
-snake = Snake()
-screen.onkey(snake.right, "d")
-screen.onkey(snake.up, "w")
-screen.onkey(snake.left, "a")
-screen.onkey(snake.down, "s")
-
-while True:
-    snake.move()
-    screen.update()
-    time.sleep(0.07)
+def set_controllers(snake, screen):
+    screen.onkey(snake.right, "d")
+    screen.onkey(snake.up, "w")
+    screen.onkey(snake.left, "a")
+    screen.onkey(snake.down, "s")
 
 
-screen.exitonclick()
+def main():
+    snake = Snake()
+    screen = set_screen()
+    set_controllers(snake=snake, screen=screen)
+
+    game_is_on = True
+    while game_is_on:
+        snake.move()
+        screen.update()
+        time.sleep(0.07)
+
+
+
+    screen.exitonclick()
+
 
 if __name__ == "__main__":
     main()
