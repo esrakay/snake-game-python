7f0df9791879bbeff0e30fb2117db08036406f1e
Esra Akay <esra.akay67@icloud.com>
2024-09-17 03:26:57 +0200

    Change snake body

--- snake_game/classes/scoreboard.py
+++ snake_game/classes/scoreboard.py
@@ -3,6 +3,7 @@ from turtle import Turtle
 ALIGNMENT = "center"
 FONT = ("Arial", 16, "normal")
 
+
 class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
--- snake_game/classes/snake_body.py
+++ snake_game/classes/snake_body.py
@@ -4,6 +4,6 @@ from turtle import Turtle
 class SnakeBody(Turtle):
     def __init__(self):
         super().__init__()
-        self.shape("square")
+        self.shape("circle")
         self.penup()
         self.color("white")
